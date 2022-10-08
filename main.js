3c29e886c1e3a057e4af6aa290b3a1c1736feb8c
Austin Amani <amaniaustin123@gmail.com>
2022-10-08 15:16:47 +0300

    Finished the Footer,CTA and Hamburger Menu

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-import './style.css'
+import './style.css';
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
+
+btn.addEventListener('click', () => {
+	btn.classList.toggle('open');
+	nav.classList.toggle('flex');
+	nav.classList.toggle('hidden');
+});
