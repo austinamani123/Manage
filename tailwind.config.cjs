8262591c25be35d0aa42037690c1cb8da003b1c0
Austin Amani <45035115+austinamani123@users.noreply.github.com>
2022-10-08 09:56:24 +0300

    Revert "Many changes to HTML,CSS and Config file (#5)"
    This reverts commit 706ef1004f8c04bf8c2f61c83b2dcc30378336e1.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,24 +1,16 @@
-/** @type {{import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./index.html'],
 	theme: {
-		screens: {
-			sm: '480px',
-			md: '768px',
-			lg: '976px',
-			xl: '1440px',
+		fontFamily: {
+			ubuntu: 'Ubuntu, sans-serif',
 		},
-		extend: {
-			colors: {
-				brightRed: 'hsl(12, 88%, 59%)',
-				brightRedLight: 'hsl(12, 88%, 69%)',
-				brightRedSupLight: 'hsl(12, 88%, 95%)',
-				darkBlue: 'hsl(228, 39%, 23%)',
-				darkGrayishBlue: 'hsl(227, 12%, 61%)',
-				veryDarkBlue: 'hsl(233, 12%, 13%)',
-				veryPaleRed: 'hsl(13, 100%, 96%)',
-				veryLightGray: 'hsl(0, 0%, 98%)',
-			},
-		},
+		extend: {},
 	},
+	plugins: [
+		require('@tailwindcss/aspect-ratio'),
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/line-clamp'),
+		require('@tailwindcss/typography'),
+	],
 };
